4041fc91f50c3d30ad132a90b486972dd2fa51fb
burak <burak@BURAK>
2023-08-17 15:02:02 +0300

    Proje dosyası ekle.

---
+++ FirsExample/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("");
+//// Değer Tipler , Referans Tipler
+//// Değer Tipler: bool (doğruluk 
+//int sayi1 = 10;
+//Console
+//// int sayi2; 
+//Console.WriteLine(sayi2);
+
+
+
+//todo: iki tane değişken olsun ve bu 2 değişkeni toplasın ekrana bastırsın.
+
+//int sayi = 1;
+//int num = 5;
+//Console.WriteLine(sayi + num);
+
+
+//string example = "olmak ya da olmamak";
+//Console.WriteLine(example.ToUpper())
+
+//int x = 10;
+//int y = 20;
+//int toplam = x + y;
+//int çıkar = x - y;
+//int çarp = x * y;
+//int böl = x / y;
+//Console.WriteLine("toplam: " + toplam);
+//Console.WriteLine("çıkar: " + çıkar);
+//Console.WriteLine("toplam: " + çarp);
+//Console.WriteLine("toplam: " + böl);
+
+//double pi, çevre, alan, r;
+//pi = 3.14;
+//r = 5;
+//çevre =2* pi * r;
+//alan = pi * (r * r);
+//Console.WriteLine("çevre:" +çevre);
+//Console.WriteLine("alan:" + alan);
